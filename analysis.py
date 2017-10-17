b5ecc87dfffc3b7fe3dcf6422aea05b1511aa142
Healthy Imaging <healthyimaging@Healthys-MacBook.local>
2017-10-17 01:38:09 -0400

    Add some options and searching by day/weekday

---
+++ analysis.py
@@ -0,0 +1,62 @@
+from datetime import datetime as dt
+from datetime import timedelta as td
+from datetime import timezone as tz
+
+def utc2est(dts):
+	OFFSET = td(hours=-4) # [TODO] handle DST
+	if type(dts) is not list:
+		return dts + OFFSET
+	return [d + OFFSET for d in dts]
+
+def f_weekday(weekday, dts):
+	return [d for d in dts if d.weekday() == weekday]
+
+def f_weekdays(dts):
+	return [d for d in dts if d.weekday() <= 4]
+
+def f_time(from_hour, to_hour, dts):
+	return [d for d in dts if from_hour <= d.hour < to_hour]
+
+def f_mornings(dts):
+	return f_time(0, 12, dts)
+
+def get_next_noon_of(weekday, datetime=dt.now(tz.utc)):
+	if datetime.weekday() == weekday and datetime.hour >= 12:
+		datetime += td(days=1)
+	while datetime.weekday() != weekday:
+		datetime += td(days=1)
+	return dt(datetime.year, datetime.month, datetime.day, 12, tzinfo=tz.utc)
+
+def group_by_week(weekday, datetimes):
+	datetimes = sorted(datetimes)
+	datetime = datetimes.pop()
+	week_cutoff = get_next_noon_of(weekday, datetime)
+	buckets = [[datetime]]
+	while len(datetimes) > 0:
+		datetime = datetimes.pop()
+		new_cutoff = get_next_noon_of(weekday, datetime)
+		if new_cutoff != week_cutoff:
+			week_cutoff = new_cutoff
+			buckets.append([])
+		buckets[-1].append(datetime)
+	return [len(b) for b in buckets]
+
+def group_by_weekday(datetimes):
+	buckets = [[], [], [], [], [], [], []]
+	for datetime in datetimes:
+		buckets[datetime.weekday()].append(datetime)
+	return [len(b) for b in buckets]
+
+def group_by_day(datetimes, include_days=range(7)):
+	datetimes = sorted(datetimes)
+	datetime = datetimes.pop()
+	current_date = datetime.date()
+	buckets = [[datetime]]
+	while len(datetimes) > 0:
+		datetime = datetimes.pop()
+		while current_date > datetime.date():
+			current_date -= td(days=1)
+			if current_date.weekday() in include_days:
+				buckets.append([])
+		buckets[-1].append(datetime)
+	return [len(b) for b in buckets]
--- predict.py
+++ predict.py
@@ -1,8 +1,25 @@
+"""PredictIt analysis.
+
+Usage:
+  predict [--vp1 --vp2 --rdt --potus] [--all --week --day --weekday] [(-w <weekday>)] [(-t <from> <to>)] [(-f <filters>)...] [-c | --cached]
+  predict ship <name> move <x> <y> [--speed=<kn>]
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+
+"""
+from docopt import docopt
+
 import json
 from datetime import datetime as dt
 from datetime import timedelta as td
 from datetime import timezone as tz
 from pprint import pprint as pp
+import pickle
 
 import twitter
 api = twitter.Api(consumer_key='s7rxM3EtQxWRicsFyq16yMOQo',
@@ -10,10 +27,11 @@ api = twitter.Api(consumer_key='s7rxM3EtQxWRicsFyq16yMOQo',
 	access_token_key='1043992880-6yvvOgaUU389q2sK9LgWRRggFimvZVy8por8bBL',
 	access_token_secret='3A05LRtPO6BBax6L0JWg2E2bBx34lXbKYTVaeXcDPjwDg')
 
-DT_FMT = '%a %b %d %H:%M:%S %z %Y'
-NOON_FRI = dt(2017, 9, 29, 16, tzinfo=tz.utc) # [TODO] handle EST/EDT instead of 16 placeholder
+import analysis
 
-def fetch_all_timestamps(screen_name='vp', max_id=None, count=None):
+DT_FMT = '%a %b %d %H:%M:%S %z %Y'
+
+def fetch_all_timestamps(screen_name='vp', max_id=None, count=3000):
 	data = []
 	while count is None or len(data) < count:
 		page = api.GetUserTimeline(screen_name=screen_name, count=200, max_id=max_id)
@@ -25,41 +43,89 @@ def fetch_all_timestamps(screen_name='vp', max_id=None, count=None):
 	timestamps = [s['created_at'] for s in statuses]
 	return timestamps
 
-def get_next_noon_of(weekday, datetime=dt.now(tz.utc)):
-	if datetime.weekday() == weekday and datetime.hour >= 16:
-		datetime += td(days=1)
-	while datetime.weekday() != weekday:
-		datetime += td(days=1)
-	return dt(datetime.year, datetime.month, datetime.day, 16, tzinfo=tz.utc)
+if __name__ == '__main__':
+	args = docopt(__doc__)
+	print(args)
 
-def group_by_week(weekday, datetimes):
-	datetimes = sorted(datetimes)
-	datetime = datetimes.pop()
-	week_cutoff = get_next_noon_of(weekday, datetime)
-	buckets = [[datetime]]
-	while len(datetimes) > 0:
-		datetime = datetimes.pop()
-		new_cutoff = get_next_noon_of(weekday, datetime)
-		if new_cutoff != week_cutoff:
-			week_cutoff = new_cutoff
-			buckets.append([])
-		buckets[-1].append(datetime)
-	return buckets	
+	if args['--all']:
+		args['--week'] = True
+		args['--day'] = True
+		args['--weekday'] = True
 
-dts_vp = [dt.strptime(t, DT_FMT) for t in fetch_all_timestamps('vp')]
-dts_potus = [dt.strptime(t, DT_FMT) for t in fetch_all_timestamps('potus')]
+	# Parse usernames to search
+	usernames = []
+	if args['--vp1'] or args['--vp2']:
+		usernames += ['vp']
+	if args['--rdt']:
+		usernames += ['realDonaldTrump']
+	if args['--potus']:
+		usernames += ['potus']
+	if not any([args[u] for u in ['--vp1', '--vp2', '--rdt', '--potus']]):
+		usernames = ['vp', 'realDonaldTrump', 'potus']
 
-print(len([d for d in dts_vp if d > NOON_FRI]))
+	# Load/fetch tweet data
+	dts = {}
+	if args['-c']:
+		for username in usernames:
+			with open(username + '.pkl', 'rb') as f:
+				dts[username] = pickle.load(f)
+	else:
+		for username in usernames:
+			dts[username] = [dt.strptime(t, DT_FMT) for t in fetch_all_timestamps(username)]
+			with open(username + '.pkl', 'wb') as f:
+				pickle.dump(dts[username], f)
+	# [TODO] handle EST conversion better
+	for username in usernames:
+		dts[username] = [analysis.utc2est(d) for d in dts[username]]
 
-b1 = [len(x) for x in group_by_week(1, dts_vp)]
-b2 = [len(x) for x in group_by_week(4, dts_vp)]
-b3 = [len(x) for x in group_by_week(1, dts_potus)]
+	# Apply filters
+	for username in usernames:
+		print('Filtering {} from {}...'.format(username, len(dts[username])))
+		if 'weekdays' in args['<filters>']:
+			dts[username] = analysis.f_weekdays(dts[username])
+		if 'mornings' in args['<filters>']:
+			dts[username] = analysis.f_mornings(dts[username])
+		if str(args['<weekday>']).isdigit():
+			args['<weekday>'] = int(args['<weekday>'])
+			dts[username] = analysis.f_weekday(args['<weekday>'], dts[username])
+		if str(args['<from>']).isdigit() and str(args['<to>']).isdigit():
+			args['<from>'] = int(args['<from>'])
+			args['<to>'] = int(args['<to>'])
+			dts[username] = analysis.f_time(args['<from>'], args['<to>'], dts[username])
+		print('to {}!'.format(len(dts[username])))
 
-print('vp Tuesday')
-print(b1)
+	# Analyses [TODO] merge these with dispatch
+	by_week = {}
+	if args['--week']:
+		if args['--vp1']: by_week['vp T'] = analysis.group_by_week(1, dts['vp'])
+		if args['--vp2']: by_week['vp F'] = analysis.group_by_week(4, dts['vp'])
+		if args['--rdt']: by_week['rdt W'] = analysis.group_by_week(2, dts['realDonaldTrump'])
+		if args['--potus']: by_week['potus T'] = analysis.group_by_week(1, dts['potus'])
 
-print('vp Friday')
-print(b2)
+	by_weekday = {}
+	if args['--weekday']:
+		if args['--vp1'] or args['--vp2']: by_weekday['vp'] = analysis.group_by_weekday(dts['vp'])
+		if args['--rdt']: by_weekday['rdt'] = analysis.group_by_weekday(dts['realDonaldTrump'])
+		if args['--potus']: by_weekday['potus'] = analysis.group_by_weekday(dts['potus'])
 
-print('potus Tuesday')
-print(b3)
+	by_day = {}
+	if args['--day']:
+		if args['--vp1'] or args['--vp2']: by_day['vp'] = analysis.group_by_day(dts['vp'])
+		if args['--rdt']: by_day['rdt'] = analysis.group_by_day(dts['realdDonaldTrump'])
+		if args['--potus']: by_day['potus'] = analysis.group_by_day(dts['potus'])
+
+	for label, datetimes in dts.items():
+		print(label)
+		print(len(datetimes))
+
+	for label, data in by_week.items():
+		print(label)
+		print(data)
+
+	for label, data in by_weekday.items():
+		print(label)
+		print(data)
+
+	for label, data in by_day.items():
+		print(label)
+		print(data)
